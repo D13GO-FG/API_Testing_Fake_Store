612c8c6444c6756f8e757591f0d980f19de628e9
Diego Flores <ldiego.flores98@gmail.com>
2023-03-01 18:37:53 -0800

    Test case: Verify if endpoint to get details of specific product base in ID works as expected.

--- src/test/java/tests/createProduct.java
+++ src/test/java/tests/createProduct.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.restassured.response.Response;
 import org.fakeStore.endpoints.APIConstants;
+import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import static io.restassured.RestAssured.given;
 
 public class createProduct extends BaseTest{
     @Test
-    public void testCreateProduct(){
+    public void testCreateProduct(ITestContext context){
         Response response = given()
                 .spec(requestSpecification)
                 .body(requestPayloads.getProductPayload())
@@ -21,6 +22,8 @@ public class createProduct extends BaseTest{
         assertActions.verifyResponseBody(apiActions.getDataFromJsonPath(response, "price"), 500, "The price isn't right.");
         assertActions.verifyResponseBody((String) apiActions.getDataFromJsonPath(response, "description"), "Pijamas Unisexo Franela Adulto Cosplay Pikachu", "The description isn't right.");
         assertActions.verifyResponseBody((ArrayList) apiActions.getDataFromJsonPath(response, "images"), "https://www.dhresource.com/albu_844327653_00/temp2.0x0.jpg", "The image isn't there.");
-        System.out.println(response.body().prettyPeek());
+        int id = apiActions.getDataFromJsonPath(response, "id");
+        context.getSuite().setAttribute("id", id);
+        System.out.println("Product ID is: " + id);
     }
 }
--- src/test/java/tests/getProduct.java
+++ src/test/java/tests/getProduct.java
@@ -1,4 +1,22 @@
 package tests;
 
-public class getProduct {
+import io.restassured.response.Response;
+import org.fakeStore.endpoints.APIConstants;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class getProduct extends BaseTest{
+    @Test
+    public void testGetProduct(ITestContext context){
+        int id = (int) context.getSuite().getAttribute("id");
+        Response response = given()
+                .spec(requestSpecification)
+                .pathParams("id", id)
+                .when()
+                .get(APIConstants.getProduct);
+        assertActions.verifyStatusCode(response);
+        System.out.println(response.body().prettyPeek());
+    }
 }
